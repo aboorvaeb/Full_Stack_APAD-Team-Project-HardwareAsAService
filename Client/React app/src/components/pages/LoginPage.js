0a9beb14cb46042633db001c9f66ba69c1460669
Aboorva Erode Baskaran <aboorvaerodebaskaran@Aboorvas-MacBook-Air.local>
2022-07-31 10:05:13 -0500

    Integration done for login page

--- Client/React app/src/components/pages/LoginPage.js
+++ Client/React app/src/components/pages/LoginPage.js
@@ -1,57 +1,61 @@
 import React, { useEffect, useState }from 'react'
 import { Link } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 import '../../App.css'
+
 
 export default function SignInPage() {
 
     const [username, setUserName] = useState('');
     const [pwd, setPwd] = useState('');
-    const [response, setResponse] = useState('');
-    const [showresponse, setShowResponse] = useState(false);
+    const [response, setShowResponse] = useState(false);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log("inside handle Submit")
-        fetch('/verify_user', {
-            method: ['POST'],
-            headers: {'Content-Type': 'application/json',},
-            body: JSON.stringify({"username": username, "password": pwd })
-          })    
-            .then(response => response.json())
-            .then(data => setResponse(data.data_from_backend))
-            console.log(response)
-            setShowResponse(true);
+    function callApi() {
+      let path = "/newproject"; 
+      
+        // Simple POST request with a JSON body using fetch
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({"username": username,"pwd": pwd})
+        };
+        fetch('/verify_user', requestOptions)
+            .then(data => data.json())
+            .then(json => {
+              //alert(JSON.stringify(json)))
+              if(json.message === "success")  {window.location.href = "/home"}
+              else alert(JSON.stringify(json))
+            })
+          
+        setUserName("")
+        setPwd("")
+            
       }
-
+      
+    
+    
     return (
-        <div className="text-center m-5-auto">
-            <h2>User Login</h2>
-            <form action="/home" className="login">
+        <div className="App">
+            <form onClick="return formResponse();">
                 <p>
-                    <label>Username </label><br/>
-                    <input name="username" value={username} 
-                    onChange={(e) => setUserName(e.target.value)} />
+                <label> Input </label>
+                <br></br>
+                <input type="text" id="username" value={username}  onChange={(e) => setUserName(e.target.value)}/>
                 </p>
-                <p>
-                    <label>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forgot password?</label></Link>
-                    <br/>
-                    <input name="password" value={pwd} 
-                    onChange={(e) => setPwd(e.target.value)} />
-                </p>
-                <p>
-                <button onClick={handleSubmit} type="submit">
-                    Submit
-                </button>
-                </p>
+                <label> Password </label>
+                <br></br>
+                <input type="password" id="pwd" value={pwd} onChange={(e) => setPwd(e.target.value)} />
             </form>
-            {showresponse === true && 
-                <p>Hii {username}, Respnse: {response}</p>}
-            <footer>
-                <p>First time? <Link to="/register">Create an account</Link>.</p>
-                <p><Link to="/">Back to Homepage</Link>.</p>
-            </footer>
+
+            <div>
+              <p>
+            <button onClick={callApi}>SUBMIT</button>
+              </p>
+            </div>
         </div>
-    )
+      );
+   
 }
