14af0a56bdcfdbf2328853ec2d892d02d4f0292e
Aboorva Erode Baskaran <aboorvaerodebaskaran@wireless-10-146-34-121.public.utexas.edu>
2022-07-31 20:20:49 -0500

    projects added

---
+++ Client/React app/src/components/pages/ResourceManagementPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
+import '../../App.css'
+
+export default function ResourceManagementPage() {
+
+    return (
+        <p>
+            Hello
+        </p>
+    )
+
+}
