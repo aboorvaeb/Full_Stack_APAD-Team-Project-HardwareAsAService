51baa02faea1128f54d3d48ac8c8e0533cbf9ae0
SANJO SHAJU <sanjo3024@gmail.com>
2022-08-11 13:22:23 -0500

    add_user pwd bug fix + env vars added

--- Server/app.py
+++ Server/app.py
@@ -5,12 +5,18 @@ import json
 from bson import json_util
 import flask
 from flask_cors import CORS, cross_origin
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 app = flask.Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-app.config["MONGO_URI"] = "mongodb+srv://admin2022:scrumbledore2022@cluster0.ofcrc.mongodb.net/ResourceManagement?retryWrites=true&w=majority"
+db_password = os.environ.get('pwd')
+db_username = os.environ.get('username')
+app.config["MONGO_URI"] = "mongodb+srv://"+db_username+":"+db_password+"@cluster0.ofcrc.mongodb.net/ResourceManagement?retryWrites=true&w=majority"
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
 
@@ -33,9 +39,11 @@ def customEncrypt(text,d): #text is the input string, n is the number of places 
 @cross_origin()
 def add_user():
     __req_body = flask.request.get_json()
+    if (__req_body['pwd'] == ""):
+        return flask.jsonify(message="Please enter a password!", success=False)
     encrypted_pwd = customEncrypt(__req_body['pwd'],1)
     __user = db.Users.find_one({"username":__req_body["username"]})
-    print(__user)
+    
     if __user is None:
         db.Users.insert_one({'username': __req_body['username'], 'pwd': encrypted_pwd, 'projects':[]})
         return flask.jsonify(message="success", success=True)
@@ -51,41 +59,11 @@ def get_users():
     for document in __user:
         document.pop('pwd', None)
         document.pop('_id', None)
-        # json_doc = json.dumps(document, default=json_util.default)
-        # print(document)
+        
         json_docs.append(document['username'])
-    # json_docs = json.dumps(json_docs, default=json_util.default)
-    # print(json_docs)
+        
     response = {"username":json_docs}
     return json.dumps(response)
-
-# # Verify login. Checks username and password entered
-# @app.route("/verify_user", methods=['POST'])
-# @cross_origin()
-# def verify_user():
-#     __req_body = flask.request.get_json()
-#     __user = db.Users.find({})
-#     for document in __user:
-#         if (__req_body['username'] == document['username']) and (__req_body['pwd'] == document['pwd']):
-#             return flask.jsonify(message="success")
-#     return flask.jsonify(message="failed")
-
-# # Verify login. Checks username and password entered
-# @app.route("/verify_user", methods=['POST'])
-# @cross_origin()
-# def verify_user():
-#     __req_body = flask.request.get_json()
-#     __user = db.Users.find({})
-#     for document in __user:
-#         if (__req_body['username'] == document['username']) and (__req_body['pwd'] == document['pwd']):
-#             if "projects" not in document.keys():
-#                 projects = []
-#             else:
-#                 projects = document['projects'] if len(document["projects"])>0 else []
-
-#             response={"message":"success","projects":projects}
-#             return json.dumps(response)
-#     return flask.jsonify(message="failed")
 
 # Add project
 @app.route("/add_project", methods=['POST'])
@@ -114,22 +92,6 @@ def add_project():
     else:
         return flask.jsonify(message="Project ID: " + __req_body['projectid'] + " already exists!", success=False)
 
-# # Get list of all projects
-# @app.route("/get_allprojects", methods=['GET'])
-# @cross_origin()
-# def get_allprojects():
-#     __project = db.Project.find({})
-#     json_docs = []
-#     for document in __project:
-#         document.pop('_id', None)
-#         json_doc = json.dumps(document, default=json_util.default)
-#         # print(document)
-#         json_docs.append(json_doc)
-#     # json_docs = json.dumps(json_docs, default=json_util.default)
-#     # print(json_docs)
-#     # response = {"username":json_docs}
-#     return json.dumps(json_docs)
-
 # Get list of all users
 @app.route("/get_allprojects", methods=['GET'])
 @cross_origin()
@@ -141,29 +103,11 @@ def get_allprojects():
         document.pop('users', None)
         document.pop('res_utilized', None)
         document.pop('_id', None)
-        # json_doc = json.dumps(document, default=json_util.default)
-        # print(document)
+        
         json_docs.append(document['projectid'])
-    # json_docs = json.dumps(json_docs, default=json_util.default)
-    # print(json_docs)
+        
     response = {"projects":json_docs}
     return json.dumps(response)
-
-# # Get project details. Checks projectid passed
-# @app.route("/get_project", methods=['POST'])
-# @cross_origin()
-# def get_project():
-#     __req_body = flask.request.get_json()
-#     __project = db.Project.find({})
-#     for document in __project:
-#         if (__req_body['projectid'] == document['projectid']):
-#             document.pop('_id', None)
-#             print(document)
-#             return json.dumps(document, default=json_util.default)
-
-#             # response={"message":"success","projects":projects}
-#             # return json.dumps(response)
-#     return flask.jsonify(message="Failed: No such projectid")
 
 # Get list of all projects
 @app.route("/get_hardwareset", methods=['GET'])
@@ -173,61 +117,10 @@ def get_hardwareset():
     json_docs = []
     for document in __hardwareSet:
         document.pop('_id', None)
-        # json_doc = json.dumps(document, default=json_util.default)
-        # print(document)
+        
         json_docs.append(document)
-    # json_docs = json.dumps(json_docs, default=json_util.default)
-    # print(json_docs)
-    # response = {"username":json_docs}
+        
     return json.dumps(json_docs, default=json_util.default)
-
-# # Check-in operation
-# @app.route("/resource_management", methods=['POST'])
-# @cross_origin()
-# def resource_management():
-#     print("resource_management")
-#     __req_body = flask.request.get_json()
-#     print(__req_body)
-#     __hardwareSet = db.HardwareSet.find({})
-#     __project = db.Project.find({})
-
-#     for HWSet in __req_body["requestvalue"]:
-#         print("-----------")
-#         print(HWSet)
-#         __HWSet_key = HWSet['resourceid']
-#         print(__HWSet_key)
-#         print("HARDWARE")
-#         for document in __hardwareSet:
-#             if (document['resourceid'] == __HWSet_key):
-#                 if(__req_body["operation"]=="check-in"):
-#                     __availability = document["availability"] + HWSet["value"]
-#                 else:
-#                     __availability = document["availability"] - HWSet["value"]
-#                 db.HardwareSet.find_one_and_update({"_id": document["_id"]}, 
-#                                 {"$set": {"resourceid": __HWSet_key,"capacity": document["capacity"], "availability":__availability}})
-
-#         print("PROJECT")
-#         for document in __project:
-#             print(document)
-#             if(__req_body["projectid"]==document["projectid"]):
-#                 print(document)
-#                 __res_utilized = document["res_utilized"]
-#                 print(__res_utilized)
-#                 if __HWSet_key not in __res_utilized.keys():
-#                     print("if the key is not there")
-#                     __res_utilized.update({__HWSet_key:0})
-#                 if(__req_body["operation"]=="check-in"):
-#                     print("check-in")
-#                     __updatedUtilized = __res_utilized[__HWSet_key] - HWSet["value"]
-#                 else:
-#                     __updatedUtilized = __res_utilized[__HWSet_key] + HWSet["value"]
-#                 __res_utilized.update({__HWSet_key:__updatedUtilized})
-
-#                 print(__res_utilized)
-#                 db.Project.find_one_and_update({"_id": document["_id"]}, 
-#                                 {"$set": {"projectid":document["projectid"], "projectdesc":document["projectdesc"],"users":document["users"],"res_utilized":__res_utilized}})
-
-#     return flask.jsonify(message="success")
 
 # Get project details. Checks projectid passed
 @app.route("/get_project", methods=['POST'])
@@ -293,20 +186,6 @@ def verify_user():
 
 
 
-    # __user = db.Users.find({})
-    # for document in __user:
-    #     if (__req_body['username'] == document['username']) and (__req_body['pwd'] == document['pwd']):
-    #         if "projects" not in document.keys():
-    #             projects = []
-    #         else:
-    #             projects = document['projects'] if len(document["projects"])>0 else []
-
-    #         response={"message":"success","projects":projects}
-    #         return json.dumps(response)
-    # return flask.jsonify(message="failed")
-
-
-
 # Check-in or check-oot operation
 @app.route("/resource_management", methods=['POST'])
 @cross_origin()
@@ -341,13 +220,6 @@ def resource_management():
             __res_utilized.update({__hardwareSet["resourceid"]:__updatedUtilized})
 
             Project_update.append({"_id": __project["_id"], "set":{"projectid":__project["projectid"], "projectdesc":__project["projectdesc"],"users":__project["users"],"res_utilized":__res_utilized}})
-
-            # Update both Hardware and Projects collection
-            # db.HardwareSet.find_one_and_update({"_id": __hardwareSet["_id"]}, 
-            #                 {"$set": {"resourceid": __hardwareSet["resourceid"],"capacity": __hardwareSet["capacity"], "availability":__availability}})
-
-            # db.Project.find_one_and_update({"_id": __project["_id"]}, 
-            #                 {"$set": {"projectid":__project["projectid"], "projectdesc":__project["projectdesc"],"users":__project["users"],"res_utilized":__res_utilized}})
 
         # Update both Hardware and Projects collection
         for docs in HWSET_update:
